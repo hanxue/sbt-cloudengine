2467c6ef481aab088ca520a85966508b776fe336
Lee Hanxue <hanxue.developer@gmail.com>
2014-01-14 15:37:14 +0800

    Follow the style from sbt-git

--- src/main/scala/CloudEnginePlugin.scala
+++ src/main/scala/CloudEnginePlugin.scala
@@ -3,16 +3,17 @@ package sbtcloudengine
 import sbt._
 import scala.io.Source
 
-object Plugin extends sbt.Plugin {
+object SbtCloudEngine extends sbt.Plugin {
   import Keys._
   import Def.Initialize
 
   
   object CloudEngineKeys {
     lazy val login    = InputKey[Unit]("gcloud auth login", "Authenticate and login to GCE.")
-    lazy val listinstances = InputKey[Unit]("gcutil listinstances", "List active Compute Engine instances.")
+    lazy val list_instances = InputKey[Unit]("gcutil listinstances", "List active Compute Engine instances.")
     // lazy val devServer      = InputKey[revolver.AppProcess]("appengine-dev-server", "Run application through development server.")
     //lazy val stopDevServer  = TaskKey[Unit]("appengine-stop-dev-server", "Stop development server.")
+    lazy val listinstances = TaskKey[Unit]("gcutil-listinstances", "Command to list GCE instances.")
     
     
     lazy val sdkVersion     = SettingKey[String]("cloudengine-sdk-version")
@@ -30,8 +31,38 @@ object Plugin extends sbt.Plugin {
   }
   private val gce = CloudEngineKeys
   
-  object CloudEngine {
-    // see https://github.com/jberkel/android-plugin/blob/master/src/main/scala/AndroidHelpers.scala
+  object CloudEngineCommand {
+    val action: (State, Seq[String]) => State = { (state, args) =>
+      val extracted = Project.extract(state)
+      import extracted._
+      //TODO: Replace listinstances with something generic
+      val (state2, runner) = extracted.runTask(CloudEngineKeys.listinstances, state)
+      val result = gcloudRunner(args, state2.log)
+      println(result)
+      state2
+    }
+
+    // <arg> is the suggestion printed for tab completion on an argument
+    val gcloudCommand: Command = Command.args("gcloud", "<args>")(action)
+    val gcutilCommand: Command = Command.args("gcutil", "<args>")(action)
+
+    import Keys._
+    //override val settings = Seq(
+      commands += CloudEngineCommand.gcloudCommand
+      commands += CloudEngineCommand.gcutilCommand
+    //)
+
+    def gcloudRunner(args: Seq[String], log: Logger = ConsoleLogger()): String = {
+      val gceLogger = new GCELogger(log)
+      val cmd = Seq("gcloud") ++ args
+      log.debug("Command to execute: " + cmd.mkString(" "))
+      val code = sbt.Process(cmd) ! gceLogger
+      val result = gceLogger.flush(code)
+      if(code != 0)
+        sys.error("Nonzero exit code (" + code + ") running gcloud.")
+      else
+        result
+    }
     def gcloudTask(action: String,
                    depends: TaskKey[File] = gce.emptyFile, outputFile: Option[String] = None): Initialize[InputTask[Unit]] =
       Def.inputTask {
@@ -99,12 +130,12 @@ object Plugin extends sbt.Plugin {
 
         
     //gce.requestLogs     := CloudEngine.gcloudTask("request_logs", outputFile = Some("request.log")).evaluated,
-    gce.login           := CloudEngine.gcloudTask("auth login").evaluated,
-    gce.listinstances   := CloudEngine.gcutilTask("listinstances").evaluated,
+    gce.login           := CloudEngineCommand.gcloudTask("auth login").evaluated,
+    gce.list_instances   := CloudEngineCommand.gcutilTask("listinstances").evaluated,
 
 
-    gce.sdkVersion <<= (gce.libUserPath) { (dir) => CloudEngine.buildSdkVersion(dir) },
-    gce.sdkPath := CloudEngine.buildCloudengineSdkPath,
+    gce.sdkVersion <<= (gce.libUserPath) { (dir) => CloudEngineCommand.buildSdkVersion(dir) },
+    gce.sdkPath := CloudEngineCommand.buildCloudengineSdkPath,
     
     gce.binPath <<= gce.sdkPath(_ / "bin"),
     gce.libPath <<= gce.sdkPath(_ / "lib"),
@@ -117,4 +148,34 @@ object Plugin extends sbt.Plugin {
 
   lazy val cloudengineSettings = baseCloudEngineSettings
 
+  private class GCELogger(log: Logger) extends ProcessLogger {
+    import scala.collection.mutable.ListBuffer
+    import Level.{ Info, Warn, Error, Value => LogLevel }
+
+    private val msgs: ListBuffer[(LogLevel, String)] = new ListBuffer()
+
+    def info(s: => String): Unit =
+      synchronized { msgs += ((Info, s)) }
+
+    def error(s: => String): Unit =
+      synchronized { msgs += ((Error, s)) }
+
+    def buffer[T](f: => T): T = f
+
+    private def print(desiredLevel: LogLevel)(t: (LogLevel, String)): String = t match {
+      case (Info, msg) =>
+        log.info(msg)
+        msg
+      case (Error, msg) =>
+        log.log(desiredLevel, msg)
+        msg
+    }
+
+    def flush(exitCode: Int): String = {
+      val level = if (exitCode == 0) Info else Error // reduce log level Error -> Info if exitCode is zero
+      var result = msgs map print(level)
+      msgs.clear()
+      result.mkString("\n")
+    }
+  }
 }
